16e55093b8d27e3ed0b74e108c17c4c1679eb925
Arun <mywebcompanion@gmail.com>
2016-03-14 11:27:32 +0800

    checked in for sms changes

--- server/server.js
+++ server/server.js
@@ -2,8 +2,14 @@ var loopback = require('loopback');
 var boot = require('loopback-boot');
 var path = require('path');
 var bodyParser = require('body-parser');
+var config = require('config');
+var underscore = require('underscore');
+
 
 var app = module.exports = loopback();
+// Twilio Credentials
+var accountSid = config.get('twilio.accountSid');
+var authToken = config.get('twilio.authToken');
 
 app.start = function() {
   // start the web server
@@ -21,6 +27,35 @@ app.use(bodyParser.urlencoded({extended : false}));
 app.use(bodyParser.json());
 app.get('/',function(req,res){
   res.sendFile(path.resolve(__dirname + '/../client/index.html'));
+});
+
+app.get('/sendsms', function(req,res){
+
+//require the Twilio module and create a REST client
+  var client = require('twilio')(accountSid, authToken);
+  var numberList = [
+    {
+        "name": "Arun",
+        "number" : '+6594507629'
+    },
+    {
+        "name": "Idir",
+        "number" : '+6582681713'
+    }
+  ];
+  underscore.each(numberList, function(obj){
+    client.messages.create({
+      to : obj.number,
+      from: "+13156460222",
+      body: "Dear " + obj.name + ", Great change to become a changi millionaire. complete your pending duty free shopping cart and participate ! . http://ichangi.herokuapp.com/shop-ui-add-to-cart.html"
+    }, function(err, message) {
+    if(!err)
+      console.log(message.sid);
+    else
+      console.log(err);
+    });
+  });
+
 });
 
 // Bootstrap the application, configure models, datasources and middleware.
